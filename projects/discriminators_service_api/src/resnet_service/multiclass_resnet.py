194f1591d55003b594e8bc0e83df94bff7100591
Orlando Grabiel Toledano Lopez <ogtoledano@uci.cu>
2025-02-12 12:45:04 -0500

    Add new: Update model performance

--- projects/discriminators_service_api/src/resnet_service/multiclass_resnet.py
+++ projects/discriminators_service_api/src/resnet_service/multiclass_resnet.py
@@ -21,7 +21,8 @@ class ModelWithSigmoid(nn.Module):
 class ModelMulticlassResNet:
 
     def __init__(self):
-        self.localAddress = f"""{os.environ.get("LOCAL_ADDRESS_AI_MODELS", "")}/model_weights_model_sigmoid.pth"""
+        self.localAddress = f"""{os.environ.get("LOCAL_ADDRESS_AI_MODELS", 
+                                                "")}/model_weights_model_sigmoid_todo_new.pth"""
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model_rest_net_pre = models.resnet18(pretrained=True)
         nr_filters = model_rest_net_pre.fc.in_features
@@ -31,7 +32,7 @@ class ModelMulticlassResNet:
         self.model.eval()
 
     def predict(self, sample):
-        thresholds = [0.2, 0.3, 0.15, 0.25]
+        thresholds = [0.45, 0.25, 0.35, 0.55] # 0.2, 0.3, 0.15, 0.25 (old version)
 
         with torch.no_grad():
             yhat = self.model(sample)
@@ -39,15 +40,23 @@ class ModelMulticlassResNet:
 
             for i in range(predictions.shape[0]):
                 predicted_classes = []
+                predicted_classes_ponderations = []
                 for j in range(predictions.shape[1]):
                     if predictions[i][j] > thresholds[j]:
                         predicted_classes.append(j)
+                        predicted_classes_ponderations.append(predictions[i][j])
                 if len(predicted_classes) == 0:
                     predicted_class = 2
                 elif len(predicted_classes) == 1:
                     predicted_class = predicted_classes[0]
                 elif len(predicted_classes) > 1 and 2 not in predicted_classes:
-                    predicted_class = 4
+                    predicted_classes_ponderations = np.array(predicted_classes_ponderations)
+                    score = np.sum(predicted_classes_ponderations)
+                    if score > 1.35:  # 1.35
+                        predicted_class = 4
+                    else:
+                        max_index = np.argmax(predictions[i])
+                        predicted_class = max_index
                 else:
                     max_index = np.argmax(predictions[i])
                     predicted_class = max_index
